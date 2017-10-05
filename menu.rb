1750d31fbf784fd0654c303891ebfe3394d99d41
anjarupnik <anjarupnik@hotmail.com>
2017-10-05 21:17:52 +0200

    add pizza to order

--- menu.rb
+++ menu.rb
@@ -19,4 +19,12 @@ def print
     puts "#{pizza.number}: #{pizza.name} - #{pizza.ingredients}"
   end
 end
+
+def make_choice
+
+  puts "Which one would you like?"
+  pizza_number = gets.chomp.to_i
+
+  @pizzas.find {|pizza| pizza.number == pizza_number}
+end
 end
--- pizza_delivery.rb
+++ pizza_delivery.rb
@@ -1,6 +1,8 @@
 require_relative "menu.rb"
+require_relative "order.rb"
 
 menu = Menu.new
+order = Order.new
 done = false
 
 puts "Hello, welcome to our pizza express delivery"
@@ -9,6 +11,7 @@ while not done
   puts "What can I do for you?"
   puts "1: Order a pizza"
   puts "2: Nothing"
+  puts "3: Check out (€ #{order.value})" if order.value > 0
   puts "----------------------------"
 
   choice = gets.chomp.to_i
@@ -16,6 +19,8 @@ while not done
   when 1
     puts "Let me show you the menu\n"
     menu.print
+    pizza = menu.make_choice
+    order.add(pizza)
   when 2
     done = true
   else
